0c9b0f64164c7fb177bc37a7741e4757b82fabc5
oleg <egormaysnikov@gmail.com>
2023-04-06 20:09:24 +0300

    fix add user

--- server/middleware/ifRole.js
+++ server/middleware/ifRole.js
@@ -2,8 +2,10 @@ const jwt = require('jsonwebtoken')
 
 module.exports = function(checkingRole) {
     return function (req, res, next) {
-        const token = req.headers['authorization'].split(' ')[1]// Bearer asfasnfkajsfnjk
+        console.log('authorization', req.headers['authorization']);
         console.log("req.cookies",req.cookies)
+
+        const token = req.headers['authorization'].split(' ')[1]// Bearer asfasnfkajsfnjk
         console.log("token: ", token)
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
         console.log(decoded, checkingRole)
